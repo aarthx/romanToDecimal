18fb004912870116decb5891035fbbe8135ad62b
Arthur <arthuraugusto.exe@gmail.com>
2022-05-13 22:54:00 -0300

    HTML e CSS inicial

---
+++ script.js
@@ -0,0 +1,11 @@
+
+//contar letras e multiplicar por milhar, centena, dezena e unidade
+function converteRomano() {
+    var input = document.querySelector("#input");
+    var output = document.querySelector("#output");
+    var letrasInput = Array.from(input.value)
+
+    if (!letrasInput.includes('I' | 'V' | 'X' | 'L' | 'C' |'D' |'M')) {
+
+    } 
+}
